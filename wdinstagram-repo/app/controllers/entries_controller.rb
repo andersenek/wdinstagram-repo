5eda838c9c0a09c1c838491fd83a1d1f173c27c9
Erica Andersen <andersenek@gmail.com>
2015-07-27 21:09:56 -0400

    Fixed route instance variables and added 'edit' and 'show' views.

--- wdinstagram-repo/app/controllers/entries_controller.rb
+++ wdinstagram-repo/app/controllers/entries_controller.rb
@@ -5,4 +5,27 @@ class EntriesController < ApplicationController
     @entries = Entry.all
   end
 
+# show
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+# create
+  def create
+    @entry = Entry.create(params[:entry])
+    redirect_to("/entries/#{@entry.id}")
+  end
+
+# edit
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+# update
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.update(params[:entry])
+    redirect_to("/entries/#{@entry.id}")
+  end
+
 end
