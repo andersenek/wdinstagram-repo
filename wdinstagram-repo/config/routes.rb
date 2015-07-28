6d425582d610e4703b3d3529c31aa56923936421
Erica Andersen <andersenek@gmail.com>
2015-07-27 21:00:42 -0400

    Added 'new' view and all routes for entries.

--- wdinstagram-repo/config/routes.rb
+++ wdinstagram-repo/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # routes + controller action in sinatra (artists)
   get "entries" => "entries#index" # index
+  get "entries/new" => "entries#new" # new
+  post "entries" => "entries#create" # create
+  get "entries/:id" => "entries#show" # show
+  get "entries/:id/edit" => "entries#edit" # edit
+  put "entries/:id" => "entries#update" # update
+  delete "entries/:id" => "entries#destroy" # destroy
+
 end
